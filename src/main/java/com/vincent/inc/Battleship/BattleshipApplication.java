a9d66a532379476fcfa4a747f1deb07973fe86e4
VincentB00 <vincentbui1999@gmail.com>
2023-04-27 13:08:02 -0400

    init

---
+++ src/main/java/com/vincent/inc/Battleship/BattleshipApplication.java
@@ -0,0 +1,13 @@
+package com.vincent.inc.Battleship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattleshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BattleshipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vincent/inc/Battleship/BattleshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vincent.inc.Battleship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattleshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
