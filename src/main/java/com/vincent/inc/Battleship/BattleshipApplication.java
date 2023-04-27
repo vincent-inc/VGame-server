8439e89e6f6a70eea30145bb712dbf49b484e92c
VincentB00 <vincentbui1999@gmail.com>
2023-04-27 13:46:34 -0400

    fix env connection

--- src/main/java/com/vincent/inc/Battleship/BattleshipApplication.java
+++ src/main/java/com/vincent/inc/Battleship/BattleshipApplication.java
@@ -1,13 +1,27 @@
 package com.vincent.inc.Battleship;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@RestController
 public class BattleshipApplication {
+
+	@Value("${spring.profiles.active}")
+	private String env = "?";
 
 	public static void main(String[] args) {
 		SpringApplication.run(BattleshipApplication.class, args);
+	}
+
+	@GetMapping("/_status/healthz")
+	public String healthCheck() {
+		return String.format("Battleship %s is up and running", env);
 	}
 
 }
