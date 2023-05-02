e22253bcc452b96ab0183bd2672d35dec5c15f45
VincentB00 <vincentbui1999@gmail.com>
2023-05-02 17:10:04 -0400

    fix vault credential

--- k8s/battleship/prod_deploy.sh
+++ k8s/battleship/prod_deploy.sh
@@ -1,4 +1,4 @@
-namespace=VGame-prod
+namespace=vgame-server-prod
 microk8s kubectl delete namespace ${namespace}
 microk8s kubectl create namespace ${namespace}
 microk8s kubectl -n ${namespace} apply -f prod_deployment.yaml
