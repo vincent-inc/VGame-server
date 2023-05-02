55da813ce01c5a875c3fb0fbcdcc5313a291802d
VincentB00 <vincentbui1999@gmail.com>
2023-05-02 18:20:19 -0400

    Fix users mapping

--- src/main/java/com/vincent/inc/VGame/openfiegn/AuthenticatorClient.java
+++ src/main/java/com/vincent/inc/VGame/openfiegn/AuthenticatorClient.java
@@ -3,6 +3,7 @@ package com.vincent.inc.VGame.openfiegn;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.vincent.inc.VGame.model.authenticator.User;
 
@@ -11,7 +12,8 @@ import feign.Headers;
 @FeignClient("AUTHENTICATOR-SERVICE")
 @Headers("Content-Type: application/json")
 public interface AuthenticatorClient {
+    public final String USERS = "users";
     
-    @GetMapping("{id}")
+    @GetMapping("/" + USERS + "/{id}")
     public User getById(@PathVariable("id") int id);
 }
