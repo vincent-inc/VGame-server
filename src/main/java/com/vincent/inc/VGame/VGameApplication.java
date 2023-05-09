1ef414b379793637bc6816820a51d3a28a943804
VincentB00 <vincentbui1999@gmail.com>
2023-05-08 20:07:11 -0400

    add swagger redirect

--- src/main/java/com/vincent/inc/VGame/VGameApplication.java
+++ src/main/java/com/vincent/inc/VGame/VGameApplication.java
@@ -5,8 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -19,6 +21,11 @@ public class VGameApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VGameApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public ModelAndView redirectSwagger(ModelMap model) {
+		return new ModelAndView("redirect:/swagger-ui/index.html", model);
 	}
 
 	@GetMapping("/_status/healthz")
